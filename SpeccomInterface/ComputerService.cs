05cef690a32c51194f98da8e644fd07d502cbc61
darkknightza <34331233+darkknightza@users.noreply.github.com>
2018-05-22 15:14:15 +0700

    update file exe suscess

--- SpeccomInterface/ComputerService.cs
+++ SpeccomInterface/ComputerService.cs
@@ -19,80 +19,84 @@ namespace SpeccomInterface
 
         public string AddComputer(Computer computer)
         {
+
             try
             {
 
-       
-            var computerid = _context.Computer.SingleOrDefault(a => a.ProcessorId == computer.ProcessorId);
-            if (computerid == null)
-            {
-                _context.Computer.Add(computer);
+                string result = "";
+                var computerid = _context.Computer.SingleOrDefault(a => a.ProcessorId == computer.ProcessorId);
+                if (computerid == null)
+                {
+                    _context.Computer.Add(computer);
                     ComputerUser computerUser = new ComputerUser();
                     computerUser.ProcessorId = computer.ProcessorId;
                     computerUser.UserId = computer.UserId;
                     _context.ComputerUser.Add(computerUser);
                     _context.SaveChanges();
-            }
-            else
-            {
-                 computerid.LastUpdate = computer.LastUpdate;
-                _context.Entry(computerid).State = EntityState.Modified;
-                _context.SaveChanges();
-            }
-            //------------------------------------Remove Memory  -----------------------//
-            int m = 0;
-            int memorycount = _context.Memory.Where(s => s.ProcessorId == computer.ProcessorId).Select(x => x.ProcessorId).Count();
-            while (m < memorycount)
-            {
-                var memory = _context.Memory.Where(s => s.ProcessorId == computer.ProcessorId).FirstOrDefault();
-                _context.Memory.Remove(memory);
-                _context.SaveChanges();
-                m++;
-            }
-            //----------------------------------End Remove Memory -----------------------//
-            //-------------------------------------Remove DiskDrive-----------------------//
-            int d = 0;
-            int diskdrivecount = _context.DiskDrive.Where(s => s.ProcessorId == computer.ProcessorId).Select(x => x.ProcessorId).Count();
-            while (d < diskdrivecount)
-            {
-                var diskdrive = _context.DiskDrive.Where(s => s.ProcessorId == computer.ProcessorId).FirstOrDefault();
-                _context.DiskDrive.Remove(diskdrive);
-                _context.SaveChangesAsync();
-                d++;
-            }
-            //----------------------------------End Remove DiskDrive -----------------------//
-            //----------------------------------Remove GraphicCard -----------------------//
-            int g = 0;
-            int GraphicCardcount = _context.GraphicCard.Where(s => s.ProcessorId == computer.ProcessorId).Select(x => x.ProcessorId).Count();
-            while (g < GraphicCardcount)
-            {
-                var graphicCard = _context.GraphicCard.Where(s => s.ProcessorId == computer.ProcessorId).FirstOrDefault();
-                _context.GraphicCard.Remove(graphicCard);
-                _context.SaveChangesAsync();
-                g++;
-            }
-            //---------------------------------End Remove GraphicCard -----------------------//
-            foreach (var memory in computer.Memory)
-            {
-                memory.ProcessorId = computer.ProcessorId;
-                _context.Memory.Add(memory);
-                _context.SaveChanges();
-            }
-            foreach (var diskdrive in computer.DiskDrive)
-            {
-                diskdrive.ProcessorId = computer.ProcessorId;
-                _context.DiskDrive.Add(diskdrive);
-                _context.SaveChanges();
-            }
-            foreach (var graphiccard in computer.GraphicCard)
-            {
-                graphiccard.ProcessorId = computer.ProcessorId;
-                _context.GraphicCard.Add(graphiccard);
-                _context.SaveChanges();
-            }
-            
+                    result = "Create New User";
+                }
+                else
+                {
+                    var comuser = _context.ComputerUser.SingleOrDefault(s => s.ProcessorId == computer.ProcessorId);
+                    if (comuser != null)
+                    {
+                        computerid.LastUpdate = computer.LastUpdate;
+                        _context.Entry(computerid).State = EntityState.Modified;
+                        _context.SaveChanges();
+                        var userid = _context.User.SingleOrDefault(a => a.UserId == computer.UserId);
+                        if (userid.UserId != comuser.UserId)
+                        {
+                            _context.User.Remove(userid);
+                            _context.SaveChanges();
+                            var uid = _context.User.SingleOrDefault(a => a.UserId == comuser.UserId);
+                            result = "Update User :" + uid.UserName;
 
-                return "Suscess";
+                        }
+                        else
+                        {
+                            result = "Update User :" + userid.UserName;
+                        }
+
+                    }
+
+                }
+                //------------------------------------Remove Memory  -----------------------//
+                    var memoryall = _context.Memory.Where(s => s.ProcessorId == computer.ProcessorId);
+                    _context.Memory.RemoveRange(memoryall);
+                    _context.SaveChanges();
+                //----------------------------------End Remove Memory -----------------------//
+                //-------------------------------------Remove DiskDrive-----------------------//
+                    var diskdriveall = _context.DiskDrive.Where(s => s.ProcessorId == computer.ProcessorId);
+                    _context.DiskDrive.RemoveRange(diskdriveall);
+                    _context.SaveChanges();              
+                //----------------------------------End Remove DiskDrive -----------------------//
+                //----------------------------------Remove GraphicCard -----------------------//
+
+                var graphicCard = _context.GraphicCard.Where(s => s.ProcessorId == computer.ProcessorId);
+                _context.GraphicCard.RemoveRange(graphicCard);
+                _context.SaveChanges();
+                //---------------------------------End Remove GraphicCard -----------------------//
+                foreach (var memory in computer.Memory)
+                {
+                    memory.ProcessorId = computer.ProcessorId;
+                    _context.Memory.Add(memory);
+                    _context.SaveChanges();
+                }
+                foreach (var diskdrive in computer.DiskDrive)
+                {
+                    diskdrive.ProcessorId = computer.ProcessorId;
+                    _context.DiskDrive.Add(diskdrive);
+                    _context.SaveChanges();
+                }
+                foreach (var graphiccard in computer.GraphicCard)
+                {
+                    graphiccard.ProcessorId = computer.ProcessorId;
+                    _context.GraphicCard.Add(graphiccard);
+                    _context.SaveChanges();
+                }
+
+
+                return "Suscess " + result;
             }
             catch (Exception ex)
             {
